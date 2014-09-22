f9c92c31736dc2094673090ea71073fe607bb1bc
Marco Florian <sixofjune@gmail.com>
2014-09-22 16:27:41 -0500

    Removing comment from previoues conflict.

--- offline.php
+++ offline.php
@@ -243,11 +243,7 @@ else {
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php echo($livehelp_name); ?></title>
-<<<<<<< HEAD
-<link href="<?php echo $install_directory; ?>/style/styles.php?<?php echo('DOMAINID='.$domain_id); ?>" rel="stylesheet" type="text/css">
-=======
 <link href="<?php echo $install_directory; ?>/style/styles.php?<?php echo('DOMAINID='.$domain_id);?>" rel="stylesheet" type="text/css">
->>>>>>> master
 <script>
 window.resizeTo(490, <?php echo($form_high); ?>);
 </script>
